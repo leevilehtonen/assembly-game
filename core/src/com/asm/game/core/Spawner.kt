db13d9876f7d4e0dbf92151c4dd36d1d054d2f62
Elias Panula <elias.panula@aalto.fi>
2017-08-04 19:22:56 +0300

    Spawner and Obstacle

--- core/src/com/asm/game/core/Spawner.kt
+++ core/src/com/asm/game/core/Spawner.kt
@@ -1,13 +1,27 @@
 package com.asm.game.core
 
 import com.asm.game.objects.PhysicsGameObject
+import com.asm.game.objects.Player
+import com.badlogic.gdx.Game
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.physics.box2d.Body
 import ktx.collections.gdxListOf
+import java.util.*
 
-class Spawner(interval: Int) {
+class Spawner(val interval: Int, val mGameWorld: GameWorld) {
 
-    //Interval, in which obstacles are spawned
-    val interval = 3500
-    val spawned = gdxListOf<PhysicsGameObject>()
+    val objects = gdxListOf<PhysicsGameObject>()
 
+    fun spawnObject(){
+        var obj: PhysicsGameObject
+        val scale = 100
+        val random = Math.random() * scale
+        val playerTexture: Texture = Texture(Gdx.files.internal("GameScreen/Obstacle.png"))
+        val playerBody: Body = mGameWorld.physicsWorld.createDynamicBody(Vector2(playerTexture.width.toFloat(), playerTexture.height.toFloat()), Vector2(500f, 500f), 30f)
+        obj = Player(playerBody, playerTexture)
+        objects.add(obj)
+    }
 
 }
